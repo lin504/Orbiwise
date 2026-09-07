5bb629cd81508d3d5bf753780870c2243625212f
lin504 <lin504@users.noreply.github.com>
2026-09-07 17:17:26 +0800

    Initial commit: add Orbiwise-java (Spring Boot) and Orbiwise-python (FastAPI) projects

---
+++ Orbiwise-java/src/main/java/com/lony/OrbiwiseApplication.java
@@ -0,0 +1,13 @@
+package com.lony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbiwiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbiwiseApplication.class, args);
+    }
+
+}
---
+++ Orbiwise-java/src/test/java/com/lony/OrbiwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbiwiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
